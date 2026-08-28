cecc233cf412e78ee1513ac4a76be3a0d0df6a5f
MuhammadAliShir <105922642+MuhammadAliShir@users.noreply.github.com>
2026-08-28 09:14:24 +0500

    commit

--- app/config/database.py
+++ app/config/database.py
@@ -1,19 +1,32 @@
 import os
+
 from pymongo import MongoClient
 
 
-MONGO_URI: str = "mongodb+srv://anumkh256_db_user:sc07TzF0ueLiLqr8@cluster0.whlcuvr.mongodb.net/"
-DB_NAME: str = "employeesystem"
+MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise RuntimeError("MONGO_URI environment variable is not set")
+
+DB_NAME = os.getenv("DB_NAME", "employeesystem")
 
 client = MongoClient(MONGO_URI)
+
 db = client[DB_NAME]
 
 
 companies_collection = db["companies"]
+
 company_settings_collection = db["company_settings"]
+
 employees_collection = db["employees"]
+
 attendance_collection = db["attendance"]
+
 payrolls_collection = db["payrolls"]
+
 attendance_collection.create_index([("emp_id", 1), ("date", 1)])
+
 users_collection = db["users"]
+
 registration_otp_collection = db["registration_otp"]
